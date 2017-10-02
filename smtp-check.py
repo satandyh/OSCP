277b6b353d371c6bb1804c464adf99521567a04b
satandyh <satandyh@yandex.ru>
2017-10-03 00:25:55 +0300

    Update smtp-check.py
    working prototype
    without parallel functionality

--- smtp-check.py
+++ smtp-check.py
@@ -1,13 +1,54 @@
 #!/usr/bin/python
 
-#we will use cmd style of our script - just for practice and for usability
-import sys,getopt,smtplib,fileinput
+#we will use cmd style of our script - just for practice in python and for usability
+import sys,getopt,socket,fileinput
+
+#split cmd strings and filelines in one list
+def get_data(data,datafile):
+	result = []
+	if datafile:
+		with open(datafile) as f:
+			result = f.readlines()
+			result = [x.strip('\n') for x in result]
+	if data:
+		result.append(data)
+	return result
+
+#function "user_verify" that try to verify all users from file or cmd at one separate ip
+#need to do this function parallel
+def user_verify(ip,port,userlist):
+	result = []
+	try:
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.connect((ip,port))
+		s.recv(1024)
+	except:
+		print 'Oops can\'t connect to '+str(ip)+' with port '+str(port)
+		pass
+	for user in userlist:
+		s.send('VRFY '+user+'\r\n')
+		res = s.recv(1024)
+		if "252" in res:
+			result.append(user)
+		elif "250" in res:
+			result.append(user)
+	s.close()
+	return result
+
+#function "check_ip" calls function user_verify on all server ips from file or cmd
+#try to do it parallel
+def check_ip(ips,port,userlists):
+	result = []
+	for ip in ips:
+		users = user_verify(ip,port,userlists)
+		if users:
+			result.append([ip,users])
+	return result
 
 def main(argv):
 	server = ''
 	serverlist = ''
-	port = '25' # default
-	timeout = '5' # default
+	port = 25
 	username = ''
 	usernamelist = ''
 
@@ -20,43 +61,22 @@ def main(argv):
 		if opt == '-h':
 			print 'smtp-check.py -s <server> -S <Serverlist> -p <port> -u <username> -U <Usernamelist>'
 			sys.exit(1)
-		elif opt in ('-s','--server'):
+		if opt in ('-s','--server'):
 			server = arg
-		elif opt in ('-S','--Serverlist'):
+		if opt in ('-S','--Serverlist'):
 			serverlist = arg
-		elif opt in ('-p','--port'):
-			port = arg
-		elif opt in ('-u','--username'):
+		if opt in ('-p','--port'):
+			port = int(arg)
+		if opt in ('-u','--username'):
 			username = arg
-		elif opt in ('-U','--Usernamelist'):
+		if opt in ('-U','--Usernamelist'):
 			usernamelist = arg
-
-#old version of usage will be deleted
-			conn = smtplib.SMTP(server,port)
-			#debug mode
-			#conn.set_debuglevel(True)
-			try:
-				result = conn.verify(username)
-			finally:
-				conn.quit()
-			print result
+	
+	#main process here
+	userlist = get_data(username,usernamelist)
+	iplist = get_data(server,serverlist)
+	check = check_ip(iplist,port,userlist)
+	print check
 
 if __name__ == "__main__":
-        main(sys.argv[1:])
-
-#function that split cmd strings and filelines in one list
-def get_data(data,datafile):
-	result = []
-		if datafile:
-			with open(datafile) as f:
-				result = f.readlines()
-				result = [x.strip('\n') for x in result]
-		elif data:
-			result.append(data)
-	return result
-
-
-#define function named "user_verify" that try to verify all users from file or cmd at one separate ip
-#do this function parallel
-
-#define function named "several_ip" that will do parallel call function user_verify on all server ips from file or cmd
+	main(sys.argv[1:])
