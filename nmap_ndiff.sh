f682dcee151a2d64936a86d338448625feef6425
satandyh <satandyh@yandex.ru>
2019-01-16 13:07:41 +0300

    Update nmap_ndiff.sh
    some improve

--- nmap_ndiff.sh
+++ nmap_ndiff.sh
@@ -1,22 +1,23 @@
 #!/bin/sh
 
-TARGETS="<targets>"
-OPTIONS="-v -T4 -F -sV"
-DATE=$(date +%F)
-DIR="/opt/scans"
+targets="targets.txt"
+options="-v -T4 -F -sV"
+date=$(date +%F)
+dir="/opt/scans"
 
-cd $DIR || exit 2
+cd $dir || exit 2
+nmap $options -iL $dir/$targets -oA scan-$date > /dev/null
 
+if [ -e scan-prev.xml ] && [ -e "$dir/$targets" ]; then
 
-nmap $OPTIONS $TARGETS -oA scan-$date > /dev/null
-
-if [ -e scan-prev.xml ]; then
-    ndiff scan-prev.xml scan-$date.xml > diff-$date
+    ndiff scan-prev.xml scan-$date.xml > diff-$date.xml
 #    echo "*** NDIFF RESULTS ***"
-#    cat diff-$date
+    cat diff-$date.xml
 #    echo
 fi
 
-echo "*** NMAP RESULTS ***"
-cat scan-$date.nmap
+#echo "*** NMAP RESULTS ***"
+#cat scan-$date.nmap
 ln -sf scan-$date.xml scan-prev.xml
+
+exit 0
