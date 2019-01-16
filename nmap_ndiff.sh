c1ee6cda8befbf5ad76078381cc35d3f2b469e87
satandyh <satandyh@yandex.ru>
2019-01-16 17:32:37 +0300

    Update nmap_ndiff.sh

--- nmap_ndiff.sh
+++ nmap_ndiff.sh
@@ -6,7 +6,7 @@ date=$(date +%Y%m%d-%H%M%S)
 dir="/opt/scans"
 
 slack(){
-curl -F "file=@diff-$date" \
+curl -F "file=@diff_$date" \
     -F 'initial_comment="Internal Port Change Detected"' \
     -F "channels=#alerts" \
     -F "token=xxxx-xxxx-xxxx" https://slack.com/api/files.upload
@@ -17,8 +17,8 @@ nmap $options -iL "$targets" -oA "scan_$date" > /dev/null
 
 if [ -e scan_prev.xml ] && [ -e "$targets" ]; then
     ndiff scan_prev.xml "scan_$date.xml" > "diff_$date"
-    [ "$?" -eq "1" ] && sed -i -e 1,3d diff-$date && slack
+    [ "$?" -eq "1" ] && sed -i -e 1,3d diff_$date && slack
 fi
 
-ln -sf "scan_$date.xml" scan-prev.xml
+ln -sf "scan_$date.xml" scan_prev.xml
 exit 0
